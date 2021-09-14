a524f7fde44d2edf9a8bbc16fe47d90b0afb218e
lazaraus <lazartony00@gmail.com>
2021-09-14 16:27:24 +0530

    day 4

---
+++ Day4/Assessment1/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1
+{
+    enum Role{
+        Physician,Nurse,Admin,Patient,Nominee
+    }    
+    class User
+    {
+        private int id;
+        private string title;
+        private string firstName;
+        private string lastName;
+        private string emailID;
+        private string dob;
+        private Role role;
+    }
+}
---
+++ Day4/Indices/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indices
+{
+    class Program
+    {
+        private String[] names = new string[10];
+
+        public string this[int index]
+        {
+            get
+            {
+                return (index >= 0 && index <= 9) ? names[index] : "";
+            }
+            set
+            {
+                if(index>=0 && index <= 9)
+                {
+                    names[index] = value;
+                }
+            }
+        }
+
+        public int this[string name]
+        {
+            get
+            {
+                int index = 0;
+                while(index<10)
+                {
+                    if(names[index]==name)
+                    {
+                        return index;
+                    }
+                }
+                return -1;
+            }
+            set
+            {
+                if (value >= 0 && value <= 9)
+                {
+                    names[value] = name;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program[0] = "Hai";
+            Console.WriteLine(program[0]);
+            Console.WriteLine(program["Hai"]);
+            Console.Read();
+        }
+    }
+}
